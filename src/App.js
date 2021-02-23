ba9aa1c3cea80fd7923de040e4a6e95e2069ad9a
Shravan Chaudhari <shravanchaudhari99@gmail.com>
2021-02-23 23:33:19 +0530

    logged in confirmation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import react from 'react';
+import react, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
+
+
+  //Run a peice of code when some conditions is applied
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    const _token = hash.access_token;
+    window.location.hash = "";
+    console.log("I have a token", _token );
+    
+    if(_token){
+      setToken(_token);
+    }
+
+  }, []);
   return (
     <div className="App">
+      {
+        token?(
+          <h1>I am Logged in!</h1>
+        ):(
+          <Login />
+        )
+      }
+      
     <Login />
     </div>
   );
