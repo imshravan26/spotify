3cd85c1fe71a354a644933d11e58136f82b66c49
Shravan Chaudhari <shravanchaudhari99@gmail.com>
2021-03-02 19:52:01 +0530

    user authentication complete done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import react, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import Player from "./player";
 import { getTokenFromUrl } from './spotify';
-
+import spotifyWebApi from "spotify-web-api-js";
+import { useDataLayerValue } from './DataLayer';
+const spotify = new spotifyWebApi();
 function App() {
-  const [token, setToken] = useState(null);
-
+  // const [token, setToken] = useState(null);
+  const [{ user, token }, dispatch] = useDataLayerValue();
+  
 
   //Run a peice of code when some conditions is applied
   useEffect(() => {
     const hash = getTokenFromUrl();
     const _token = hash.access_token;
     window.location.hash = "";
-    console.log("I have a token", _token );
     
     if(_token){
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+        
+      })
+      
     }
 
   }, []);
+
   return (
     <div className="App">
-      {token ? (<h1>I am Logged in</h1>):( <Login /> )}
+      {token ? (<Player />):( <Login /> )}
     </div>
   );
 }
